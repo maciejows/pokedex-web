3b73529f2565cca8ac59dc1c0c28b83788084413
maciejows <owsianny.maciej@gmail.com>
2020-04-14 23:03:13 +0200

    Fetching pokemon description

--- src/app/models/Ability.ts
+++ src/app/models/Ability.ts
@@ -4,10 +4,13 @@ export class Ability {
   name: string;
   url: string;
   id: number;
-
+  description: string;
   constructor(properties: any) {
     this.name = properties.ability.name;
     this.url = properties.ability.url;
   }
 
+  setDescription(desc: string){
+    this.description = desc;
+  }
 }
