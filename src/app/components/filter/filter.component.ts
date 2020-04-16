b464b9c3022e634eb8345baba4232e0f0ef0c65c
maciejows <owsianny.maciej@gmail.com>
2020-04-16 15:52:11 +0200

    Filter component

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent implements OnInit {
+  @Output() filter = new EventEmitter();
+
+  typesMap: {[key: string] : string} = {
+    "normal": "#A8A878",
+    "fire": "#F08030",
+    "fighting": "#C03028",
+    "water": "#6890F0",
+    "flying": "#A890F0",
+    "grass": "#78C850",
+    "poison": "#A040A0",
+    "electric": "#F8D030",
+    "ground": "#E0C068",
+    "psychic": "#F85888",
+    "rock": "#B8A038",
+    "ice": "#98D8D8",
+    "bug": "#A8B820",
+    "dragon": "#7038F8",
+    "ghost": "#705898",
+    "dark": "#705848",
+    "steel": "#B8B8D0",
+    "fairy": "#EE99AC",
+  }
+  typeKeys: string[] =[];
+  selectedTypeOption: string = "";
+  pokemonName: string = "";
+  constructor() { }
+
+  getFilteredPage(){
+    console.log("emitting")
+    this.filter.emit(null)
+    this.pokemonName="";
+    this.selectedTypeOption="";
+  }
+
+  selectTypeOption(type: string){
+    this.selectedTypeOption = type;
+  }
+
+  ngOnInit(): void {
+    for (const element of Object.keys(this.typesMap)) {
+      this.typeKeys.push(element);
+      }
+  }
+
+}
