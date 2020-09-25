dd3242807c481417f639249fb25246ef5b5f9092
maciejows <owsianny.maciej@gmail.com>
2020-09-25 14:52:08 +0200

    Removed unnecessary css classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PokedexDisplayComponent } from './components/pokedex-display/pokedex-display.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
   { path: 'pokemons', component: PokedexDisplayComponent },
   { path: '**', redirectTo: '/pokemons' }
 ];
