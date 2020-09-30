13a75f9f4e6adc135fc41d1d91e4556039c7c581
maciejows <owsianny.maciej@gmail.com>
2020-09-30 12:26:47 +0200

    Services tests, added necessary imports in component tests

--- src/app/components/pokemon-list/pokemon-list.component.spec.ts
+++ src/app/components/pokemon-list/pokemon-list.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonListComponent } from './pokemon-list.component';
+import { StoreModule } from '@ngrx/store';
+import { filterReducer } from '@store/filter/filter.reducer';
+import { pageReducer } from '@store/page/page.reducer';
+import { pokemonReducer } from '@store/pokemon/pokemon.reducer';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { CountPagesPipe } from '@pipes/CountPages.pipe';
 
 describe('PokemonListComponent', () => {
   let component: PokemonListComponent;
@@ -8,7 +15,16 @@ describe('PokemonListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [PokemonListComponent]
+      imports: [
+        StoreModule.forRoot({
+          filter: filterReducer,
+          page: pageReducer,
+          pokemon: pokemonReducer
+        }),
+        AppRoutingModule,
+        HttpClientModule
+      ],
+      declarations: [PokemonListComponent, CountPagesPipe]
     }).compileComponents();
   }));
 
--- src/app/services/page.service.spec.ts
+++ src/app/services/page.service.spec.ts
@@ -1,15 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 import { PageService } from './page.service';
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PokemonPageService', () => {
   let service: PageService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientModule]
+    });
     service = TestBed.inject(PageService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getPage should return array of 50 pokemons', () => {
+    const dummyResult = [...Array(50).keys()];
+    service.getPage(1).subscribe((data) => {
+      expect(data.results.length).toBe(50);
+    });
+    const req = httpMock.expectOne(
+      `${service.apiUrl}/pokemon?offset=0&limit=50`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush({ results: dummyResult });
+  });
+
+  it('#getFilteredPokemons should return "ground" pokemons', () => {
+    const dummyResult = {
+      name: 'ground',
+      pokemon: [{ pokemon: { name: 'sandshrew', url: 'some url' } }]
+    };
+    service.getFilteredPokemons('ground').subscribe((data) => {
+      expect(data.name).toBe('ground');
+      expect(data.pokemon.length).toBeGreaterThan(0);
+    });
+    const req = httpMock.expectOne('https://pokeapi.co/api/v2/type/ground');
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyResult);
   });
 });
